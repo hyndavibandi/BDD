a78eb9469f5715f1d0497b58cea57218be28f456
Hyndavi Bandi <hyndavi_bandi@epam.com>
2021-11-12 01:29:03 +0530

    fc

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "8a5f49d7-967d-4d33-9fb1-c2218d0ea6e2",
+    "feature": "Ecommerce",
+    "scenario": "OnlineShopping",
+    "start": 1636659268198,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1636659268307,
+    "className": "undefined"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
